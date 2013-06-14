e3d78ef1a508b203fff8342348bbe9e474883667
hannesadollarsson <hannes@adollarson.se>
2013-06-14 11:08:33 +0200

    new version

--- check_traceroute.py
+++ check_traceroute.py
@@ -1,25 +1,39 @@
 #!/usr/bin/python
 import argparse
 import subprocess
+import sys
 
 parser = argparse.ArgumentParser(description='Input arguments')
-parser.add_argument('-H','--host',help='hostaddress',required=True)
-parser.add_argument('-h1','--hop1',help='hop1address',required=True)
+parser.add_argument('-t','--target',help='target address',required=True)
+parser.add_argument('-h2','--hop2',help='hop2 address',required=True)
 
 args = parser.parse_args()
 
-IP = args.host
-HOP1 = args.hop1
-:q
+IP = args.target
+HOP2 = args.hop2
+
 def traceroute(IP):
 	### Do traceroute
-	cli_cmd = "/usr/bin/tracepaht " + IP
+	cli_cmd = "/usr/bin/tracepath " + IP
 	process = subprocess.Popen(cli_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
 	output = process.communicate()
 	return output
 
-def check_hop1(IP, HOP1):
+def check_hop(IP, HOP2):
 	### Check traceroute result for HOP1
 	TRACE = traceroute(IP)
+	TRACE = TRACE[0]
+	TRACE = TRACE.split()
+	COUNT = 0
+	
+	for i in TRACE:
+		if i == "2:":
+			H2POS = COUNT+1
+			if TRACE[H2POS] == HOP2:
+				print "OK - Using primary path"
+				sys.exit(0)
+		COUNT += 1
+	print "CRITICAL - Using secondary path"
+	sys.exit(2)
 
-print check_hop1(IP, HOP1)
+check_hop(IP, HOP2)
