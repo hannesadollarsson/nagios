f3cd9ffb16bb1cb681f2c65b972997b0cf7200a4
hannesadollarsson <hannes@adollarson.se>
2013-11-28 21:19:20 +0100

    fixed bugs

--- check_ftp_list.py
+++ check_ftp_list.py
@@ -9,7 +9,7 @@ parser.add_argument('-H', '--host',help='host address',required=True)
 parser.add_argument('-u','--user',help='username',required=True)
 parser.add_argument('-p','--password',help='password',required=True)
 parser.add_argument('-d','--directory',help='working dir path',required=True)
-parser.add_argument('-f','--file',help='watch file',required=True)
+parser.add_argument('-f','--file',help='watch file',required=False)
 
 args = parser.parse_args()
 
@@ -33,18 +33,27 @@ try:
     ftp.cwd(DIR)
 except Exception:
     print "WARNING - Unable to change remote directory" 
-    exit(2)
     ftp.quit()
+    exit(1)
 
 # get directory listing
-DIR_LIST = ftp.nlst()
+try:
+    DIR_LIST = ftp.nlst()
+except Exception:
+        print "WARNING - unable to list content"
+        ftp.quit()
+        sys.exit(1)
 
-if FILE in DIR_LIST:
-    print "OK - file exists"
-    sys.exit(0)
-else:
-    print "WARNING - file does not exist"
-    sys.exit(1)
+# check for file if exists
+if FILE:
+    if FILE in DIR_LIST:
+        print "OK - file exists"
+        ftp.quit()
+        sys.exit(0)
+    else:
+        print "WARNING - file does not exist"
+        ftp.quit()
+        sys.exit(1)
 
 # close connection
 ftp.quit()
